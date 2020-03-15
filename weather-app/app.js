acfe9cd79d1ed0a5430bdf59310c3b246393d339
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-15 19:39:56 +0200

    Customize http requests : auto convert from json to object, request parameters and logging in to console useful information

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 //HOW TO MAKE HTTP REQUESTS
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/8ce44cf573f77471fab28f604c18e783/37.8267,-122.4233';
-request({url: url}, (error, response)=>{
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+const url = 'https://api.darksky.net/forecast/yourapihere/37.8267,-122.4233?units=si';
+request({url: url, json : true}, (error, response)=>{
+    console.log("It is currently " + response.body.currently.temperature + " degrees outside. There is " + response.body.currently.precipProbability + "% chance to rain");
+    console.log("Today's summary is : " + response.body.daily.data[0].summary);
 });
