fcb6defc39e83f88a5d4d43c862f7ac017591646
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-19 22:14:50 +0200

    Creating a search form that logs the data from API to console

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,21 @@
-console.log("Printing to client console for testing");
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error){
-            console.log(data.error);
-        } else {
-            console.log(data.location, data.forecast);
-        }
-    });
+
+//Manipulate DOM - call our API when user submits the form
+const form = document.querySelector('form');
+const search = document.querySelector('input');
+
+form.addEventListener('submit', (e)=>{
+    const address = search.value;
+
+    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+        response.json().then((data) => {
+            if (data.error){
+                console.log(data.error);
+            } else {
+                console.log(data.location, data.forecast);
+            }
+        });
+    }); 
+    
+    e.preventDefault();
 });
