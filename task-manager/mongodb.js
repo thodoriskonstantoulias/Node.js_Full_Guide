3f1bc2d365d00bc58dc48cb0830657e17138e241
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-20 23:17:58 +0200

    Getting data from MongoDb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,14 +2,14 @@
 
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
-const ObjectID = mongodb.ObjectID;
+//const ObjectID = mongodb.ObjectID;
 
 const connectionUrl = 'mongodb://localhost:27017';
 const databaseName = 'task-manager';
 
 //Using object Id 
-const id = new ObjectID();
-console.log(id.getTimestamp());
+// const id = new ObjectID();
+// console.log(id.getTimestamp());
 
 mongoClient.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if (error){
@@ -45,4 +45,17 @@ mongoClient.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: t
     //         }
     //     console.log(result.ops);
     // })
+
+    //Fetching data - one 
+    db.collection('users').findOne({name:'Kostas'}, (error, user) => {
+        if (error){
+            return console.log('Unable to fetch user');
+        }
+        console.log(user);
+    });
+
+    //Fetching data - many 
+    db.collection('users').find({name:'Ted'}).toArray((error, users) => {
+        console.log(users);
+    });
 });
