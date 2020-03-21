f3b8b452b5affd1def4b1c59c9266c9b281e09c4
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-21 14:34:47 +0200

    Inserting a second document with Mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 //Connect to MongoDB database
 mongoose.connect("mongodb://localhost:27017/task-manager-api", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 
-//Create the model 
+//Create the models 
 const User = mongoose.model('User', {
     name: {
         type:String
@@ -14,15 +14,35 @@ const User = mongoose.model('User', {
     }
 });
 
-//Insert a user
-const me = new User({
-    name: 'Ted',
-    age : 29
+const Task = mongoose.model('Tasks', {
+    description : {
+        type : String
+    },
+    completed : {
+        type: Boolean
+    }
 });
 
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error!', error);
-})
+//Insert a user
+// const me = new User({
+//     name: 'Ted',
+//     age : 29
+// });
 
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error!', error);
+// })
+
+//Insert a task 
+const task = new Task({
+    description : 'Go to supermarket',
+    completed : false
+});
+
+task.save().then(() => {
+    console.log(task);
+}).catch((error) => {
+    console.log(error);
+});
