a8c1059f31e8886bc6c1e9b9b0952ee7d8139123
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-21 15:50:06 +0200

    Mongoose data validation and transformation Part 1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,6 @@
 //All database related goes in here
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 //Connect to MongoDB database
 mongoose.connect("mongodb://localhost:27017/task-manager-api", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
@@ -7,14 +8,31 @@ mongoose.connect("mongodb://localhost:27017/task-manager-api", {useNewUrlParser:
 //Create the models 
 const User = mongoose.model('User', {
     name: {
-        type:String
+        type:String,
+        required: true
     },
     age : {
-        type:Number
+        type:Number,
+        default : 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive');
+            }
+        }
+    },
+    email : {
+        type:String,
+        required: true,
+        lowercase : true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
     }
 });
 
-const Task = mongoose.model('Tasks', {
+const Task = mongoose.model('Tasks', { 
     description : {
         type : String
     },
@@ -24,25 +42,26 @@ const Task = mongoose.model('Tasks', {
 });
 
 //Insert a user
-// const me = new User({
-//     name: 'Ted',
-//     age : 29
-// });
+const me = new User({
+    name: 'Kostas',
+    age : 36,
+    email : 'kostas@yahoo.com'
+});
 
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('Error!', error);
-// })
+me.save().then(() => {
+    console.log(me);
+}).catch((error) => {
+    console.log('Error!', error);
+})
 
 //Insert a task 
-const task = new Task({
-    description : 'Go to supermarket',
-    completed : false
-});
+// const task = new Task({
+//     description : 'Go to supermarket',
+//     completed : false
+// });
 
-task.save().then(() => {
-    console.log(task);
-}).catch((error) => {
-    console.log(error);
-});
+// task.save().then(() => {
+//     console.log(task);
+// }).catch((error) => {
+//     console.log(error);
+// });
