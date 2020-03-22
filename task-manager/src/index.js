a635459962662c56b4a7ee4451856edab47c1daa
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-22 16:01:52 +0200

    DELETE request for users and tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -79,7 +79,20 @@ app.patch('/users/:id', async (req,res) => {
         }
         res.send(user);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send(error);
+    }
+});
+
+app.delete('/users/:id', async (req,res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findByIdAndDelete(id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
 
@@ -147,7 +160,20 @@ app.patch('/tasks/:id', async (req,res) => {
         }
         res.send(task);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send(error);
+    }
+});
+
+app.delete('/tasks/:id', async (req,res) => {
+    const id = req.params.id;
+    try {
+        const task = await Task.findByIdAndDelete(id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
 
