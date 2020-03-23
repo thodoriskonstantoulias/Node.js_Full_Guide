30fa1262367500626a5a2f5478d8480a63ec0f19
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-23 22:21:54 +0200

    Image upload Part 3 - Saving image buffer to db and delete image functionality

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -173,7 +173,6 @@ router.delete('/users/me', auth, async (req,res) => {
 
 //Image upload route 
 const upload = multer({
-    dest : 'avatars',
     limits : {
         fileSize : 1000000
     },
@@ -186,10 +185,22 @@ const upload = multer({
     }
 });
 
-router.post('/users/me/avatar',upload.single('avatar'), (req,res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
+    //Get the binary format of the upload and save to db
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+
     res.send();
 }, (error,req,res,next) => {
     res.status(400).send({error : error.message});
+});
+
+router.delete('/users/me/avatar', auth, async (req,res) => {
+    //Delete the avatar from the user
+    req.user.avatar = undefined;
+    await req.user.save();
+
+    res.send();
 });
 
 module.exports = router;
