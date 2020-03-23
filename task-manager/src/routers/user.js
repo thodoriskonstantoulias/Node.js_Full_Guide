6d8d3c51e884caf23c6323a2c2f7e00afd2bb0ef
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-03-23 23:41:08 +0200

    Image upload Part 4 - Set client for showing image

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -203,4 +203,19 @@ router.delete('/users/me/avatar', auth, async (req,res) => {
     res.send();
 });
 
+//Setup route for client to show the image 
+router.get('/users/:id/avatar', async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+        res.set('Content-Type','image/jpg');
+        res.send(user.avatar);
+
+    } catch (error) {
+        res.status(400).send();
+    }
+});
+
 module.exports = router;
